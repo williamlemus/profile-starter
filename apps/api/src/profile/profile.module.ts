0b7f1a33d476a4ec06e9899711782e07e4ee6ab5
William Lemus <william.lemus@gmail.com>
2025-04-25 00:53:22 -0400

    Adding required module for guard to work, creating type of return from feed

--- apps/api/src/profile/profile.module.ts
+++ apps/api/src/profile/profile.module.ts
@@ -3,11 +3,12 @@ import { ProfileService } from './profile.service';
 import { ProfileController } from './profile.controller';
 import { ProfileRepository } from './profile.repository';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   providers: [ProfileService, ProfileRepository],
   controllers: [ProfileController],
-  imports: [PrismaModule],
+  imports: [PrismaModule, ConfigModule],
   exports: [ProfileService]
 })
 export class ProfileModule {}
--- apps/web/app/api/feed.ts
+++ apps/web/app/api/feed.ts
@@ -1,25 +1,31 @@
+import { z } from 'zod';
+import { profileResponseSchema } from './profile';
 
 // might hoist to another file
 const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
-export const getFeed = async (token: string | null) => {
-    const url = `${API_URL}/feed`;
-    try {
-      const response = await fetch(url, {
-        method: 'GET',
-        headers: {
-          Authorization: `Bearer ${token}`,
-          'Content-Type': 'application/json',
-        },
-      });
-      if (!response.ok) {
-        throw new Error(
-          `Response status: ${response.status} \n ${response.statusText}`,
-        );
-      }
-      return response.json();
-    } catch (e: unknown) {
-      console.error('Error obtaining profile!');
-      throw e;
+const profileFeedSchema = z.array(profileResponseSchema);
+
+export const getFeed = async (
+  token: string | null,
+): Promise<z.infer<typeof profileFeedSchema>> => {
+  const url = `${API_URL}/feed`;
+  try {
+    const response = await fetch(url, {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${token}`,
+        'Content-Type': 'application/json',
+      },
+    });
+    if (!response.ok) {
+      throw new Error(
+        `Response status: ${response.status} \n ${response.statusText}`,
+      );
     }
-  };
+    return response.json();
+  } catch (e: unknown) {
+    console.error('Error obtaining profile!');
+    throw e;
+  }
+};
